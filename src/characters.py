a6ea0a88ffffba16308775de2f5c5dfb2a8330e1
tatih <hazeltatianna55@gmail.com>
2026-04-23 17:17:49 -0500

    attempt to give characters actions

--- src/characters.py
+++ src/characters.py
@@ -17,6 +17,27 @@ class Zombie(Fighter):
     def __init__(self, name, health, attack_power):
         super().__init__(name, health, attack_power)
 
+    def move_towards_player(self):
+        print(f"{self.name} shambles towards the player!")
+    
+    def attack_player(self, other):
+        other.health -= self.attack_power
+        print(f"{self.name} attacks {other.name} for {self.attack_power} damage!")
+
+    
 class Player(Fighter):
     def __init__(self, name, health, attack_power):
         super().__init__(name, health, attack_power)
+
+    def move_forward(self):
+        print(f"{self.name} moves forward!")
+
+    def move_backwards(self):
+        print(f"{self.name} moves backwards!")
+
+    def jump(self):
+        print(f"{self.name} jumps!")
+
+    def attack_zombie(self, other):
+        other.health -= self.attack_power
+        print(f"{self.name} attacks {other.name} for {self.attack_power} damage!")
