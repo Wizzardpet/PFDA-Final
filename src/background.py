fcdd6500e1ce12a4ec4ed54cce6314adccd0ccca
tatih <hazeltatianna55@gmail.com>
2026-04-24 18:39:31 -0500

    complete redo after coding for text instead of images

---
+++ src/background.py
@@ -0,0 +1,9 @@
+import pygame
+import sys
+
+pygame.init()
+
+
+
+
+class title_card():
--- src/characters.py
+++ src/characters.py
@@ -2,47 +2,8 @@
 
 import images
 
-
-
-
-class Fighter():
-    def __init__(self, name, health, attack_power):
-        self.name = name
-        self.health = health
-        self.attack_power = attack_power
-
-    def attack(self, other):
-        other.health -= self.attack_power
-        print(f"{self.name} attacks {other.name} for {self.attack_power} damage!")
-
-    def is_alive(self):
-        return self.health > 0
     
-class Zombie(Fighter):
-    def __init__(self, name, health, attack_power):
-        super().__init__(name, health, attack_power)
-
-    def move_towards_player(self):
-        print(f"{self.name} shambles towards the player!")
+class Zombie():
     
-    def attack_player(self, other):
-        other.health -= self.attack_power
-        print(f"{self.name} attacks {other.name} for {self.attack_power} damage!")
-
     
-class Player(Fighter):
-    def __init__(self, name, health, attack_power):
-        super().__init__(name, health, attack_power)
-
-    def move_forward(self):
-        print(f"{self.name} moves forward!")
-
-    def move_backwards(self):
-        print(f"{self.name} moves backwards!")
-
-    def jump(self):
-        print(f"{self.name} jumps!")
-
-    def attack_zombie(self, other):
-        other.health -= self.attack_power
-        print(f"{self.name} attacks {other.name} for {self.attack_power} damage!")
+class Player():
