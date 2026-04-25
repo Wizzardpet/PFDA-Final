369895285184525521292e7015c4016ddbed934d
tatih <hazeltatianna55@gmail.com>
2026-04-24 19:05:32 -0500

    starting with bg and tc instead

--- src/project.py
+++ src/project.py
@@ -6,3 +6,13 @@
 # Exit game
 import pygame
 import sys
+
+pygame.init()
+#Making screen
+
+Width, Height = 800, 600
+screen = pygame.display.set_mode((Width, Height))
+pygame.display.set_caption("Side Scroller")
+
+title_font = pygame.font.SysFont("Arial", 64)
+menu_font = pygame.font.SysFont("Arial", 32)
