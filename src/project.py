2b3153ba587a66c43dcd5c1b5e670e8e3ca88d42
tatih <hazeltatianna55@gmail.com>
2026-04-24 19:11:16 -0500

    making the titlecard

--- src/project.py
+++ src/project.py
@@ -16,3 +16,9 @@ pygame.display.set_caption("Side Scroller")
 
 title_font = pygame.font.SysFont("Arial", 64)
 menu_font = pygame.font.SysFont("Arial", 32)
+
+class TitleScreen:
+    def __init__(self):
+        self.title_text = title_font.render("Brains and Brownies", True, (255,255,255))
+        self.prompt_text = menu_font.render("Press any key to start", True, (200,200,200))
+        self.finished = False
